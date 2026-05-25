817a06c616a544dabf64f4ddf356f92d2cb477f2
Jon's <jon.rod2405@gmail.com>
2026-05-25 23:37:35 +0000

    Ajuste dos botões

--- Uninove.web/obj/Debug/net10.0/Uninove.web.AssemblyInfo.cs
+++ Uninove.web/obj/Debug/net10.0/Uninove.web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uninove.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+795aada1ee6db2d0cb0c50c2e9014df44c9ad85c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf5cdaf42700a6ca816e490df5b8b2de47ff75d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uninove.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uninove.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
